baa85fd4d9a260d2e83962b83bedaa288c032ae6
Jimmy-Jiang-Junior <577220709@qq.com>
2022-10-23 10:01:18 +0800

    Modify stack_trace.sh

--- ftrace/stack_trace.sh
+++ ftrace/stack_trace.sh
@@ -9,24 +9,22 @@ echo > trace
 echo function > current_tracer
 
 # 设置要trace的函数, 需要确保这个函数在available_filter_function中
-echo default_idle_call > stack_trace_filter
-
-# enable stack_tracer
-echo 1  >  /proc/sys/kernel/stack_tracer_enabled
+echo default_idle_call > set_ftrace_filter
+echo 1  > options/func_stack_trace # 设置调用栈选项
 
 # 开始追踪
 echo 1 > tracing_on
 
-ls > /dev/null
+sleep 0.01
 
 # 结束追踪
 echo 0 > tracing_on
 
-cat stack_trace
+cat trace
 
-# disable stack_tracer
-echo 0  >  /proc/sys/kernel/stack_tracer_enabled
 
-echo > stack_trace_filter
+echo > set_ftrace_filter
 
 echo nop > current_tracer
+
+echo 0 > options/func_stack_trace
